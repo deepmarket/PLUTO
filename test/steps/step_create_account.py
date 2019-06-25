74427f37a33a05ed1811f543c58a87ab24781d09
samgomena <samiam4612@gmail.com>
2019-06-24 21:22:04 -0700

    feat: Create account steps.

---
+++ test/steps/step_create_account.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QDialog
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
+
+from behave import use_step_matcher, when, then
+
+from test.steps.step_helpers import assert_equal, assert_is_not, assert_is
+
+use_step_matcher("re")
+

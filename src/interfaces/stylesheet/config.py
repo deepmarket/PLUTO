8a4614f5d2463321ca5496be73ed4eec84d5f7fe
Martin <lixphilosophy@gmail.com>
2019-07-04 23:03:38 -0700

    start to refactoring the project structure, begin with widget model, created layout object class

---
+++ src/interfaces/stylesheet/config.py
@@ -0,0 +1,36 @@
+# UI Palette
+COLOR_01 = "#6C7E8E"        # standard label, background
+COLOR_02 = "#91A4AD"
+COLOR_03 = "#F4F8F9"
+COLOR_04 = "#657FEB"
+COLOR_05 = "#7DCC54"
+COLOR_06 = "#FFFFFF"
+COLOR_07 = "#D0DBE0"
+COLOR_08 = "#C6CDDB"        # disable background
+COLOR_09 = "#F2F2F2"        # disable label
+
+# System Environment variable
+QT_AUTO_SCREEN_SCALE_FACTOR = 1
+QT_SCALE_FACTOR = 2
+
+# Direction
+LEFT = "LEFT"
+RIGHT = "RIGHT"
+CENTER = "CENTER"
+
+# Image type
+PNG = ".png"
+JPG = ".jpg"
+JPEG = ".jpeg"
+SVG = ".svg"
+
+# Type
+LABEL = "QLabel"
+QLINEEDIT = "QLineEdit"
+QPUSHBUTTON = "QPushButton"
+
+# Plot type
+ELLIPSE = "QGraphicsEllipseItem"
+LINE = "QGraphicsLineItem"
+POLYGON = "QGraphicsPolygonItem"
+TEXT = "QGraphicsTextItem"
---
+++ src/interfaces/widget/layout.py
@@ -0,0 +1,55 @@
+"""
+
+    Create a Qt layout wrapper object to arrange child widgets
+    within a widget to ensure that they make good use of the available space.
+
+"""
+
+from PyQt5 import QtWidgets
+
+
+class Layout(QtWidgets.QLayout):
+
+    def __init__(self, widget, **kwargs):
+        super(Layout, self).__init__(widget) #, **kwargs)
+        
+        if not kwargs:
+            pass
+        else:
+            # Lambda func greb input args
+            get_num = lambda x : kwargs[x] if x in kwargs else 0
+            get_param = lambda x : kwargs[x] if x in kwargs else None
+
+            l_m = get_num("l_m")
+            t_m = get_num("t_m")
+            r_m = get_num("r_m")
+            b_m = get_num("b_m")
+            space = get_num("space")
+
+            # Set margin and spacing
+            self.setContentsMargins(l_m, t_m, r_m, b_m)
+            self.setSpacing(space)
+
+            name = get_param("name")
+            align = get_param("align")
+
+            # Set object name if given
+            name and self.setObjectName(name)
+
+            # Set child widgets alignment if given
+            align and self.setAlignment(align)
+
+
+class HorizontalLayout(QtWidgets.QHBoxLayout, Layout):
+    def __init__(self, widget, **kwargs):
+        super(HorizontalLayout, self).__init__(widget, **kwargs)
+
+
+class VerticalLayout(QtWidgets.QVBoxLayout, Layout):
+    def __init__(self, widget, **kwargs):
+        super(VerticalLayout, self).__init__(widget, **kwargs)
+
+
+class StackLayout(QtWidgets.QStackedLayout, Layout):
+    def __init__(self, widget, **kwargs):
+        super(StackLayout, self).__init__(widget, **kwargs)

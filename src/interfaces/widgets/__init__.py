cc34d5190ddc59a1237587af2b7ce360f63c9e65
Martin <lixphilosophy@gmail.com>
2019-07-05 19:47:08 -0700

    add import to init

--- src/interfaces/widgets/__init__.py
+++ src/interfaces/widgets/__init__.py
@@ -1,4 +1,4 @@
 from .button import Button
 from .frame import Frame
-from .label import Label, Paragraph
+from .label import Label, Paragraph, Image
 from .layout import HorizontalLayout, VerticalLayout, StackLayout

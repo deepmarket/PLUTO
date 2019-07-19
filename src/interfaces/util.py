c4bf31cc77740fcc3349e98f532a5ca7c52f6313
Martin <lixphilosophy@gmail.com>
2019-07-19 13:48:20 -0700

    place docstrings inside of def

--- src/interfaces/util.py
+++ src/interfaces/util.py
@@ -10,15 +10,15 @@ import os
 from PyQt5.QtWidgets import QLineEdit
 
 
-"""
+def load_path(path, file):
+    """
     Find a file in a given path string
     reference: https://github.com/yahoo/TensorFlowOnSpark/blob/e2f5cc45f95812d163e75b6ddb9c4661261d3bb0/tensorflowonspark/util.py#L57
 
     :param path: base path of the file
     :param file: the name of the file, include postfix
     :return: if exist file, return the path of the file, otherwise, return False
-"""
-def load_path(path, file):
+    """
     for p in path.split(os.pathsep):
         candidate = os.path.join(p, file)
     if os.path.exists(candidate) and os.path.isfile(candidate):
@@ -26,14 +26,14 @@ def load_path(path, file):
     return False
 
 
-"""
+def get_children(widget, child_type, *args):
+    """
     Find all matched child widgets belong to the type (or with given object name)
 
     :param widget: parent widget
     :param child_type: destinated child widgets type
     :args: optional, the name that child widgets currently have
     :return: list of all matched child widgets
-"""
-def get_children(widget, child_type, *args):
+    """
     # if None, children = []
     return [child for child in widget.findChildren(child_type, *args)]

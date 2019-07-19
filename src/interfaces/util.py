59b6a1729a76c5390c1ff5d2d091120eaf0e4dc0
Martin <lixphilosophy@gmail.com>
2019-07-19 13:52:42 -0700

    optim load_path param

--- src/interfaces/util.py
+++ src/interfaces/util.py
@@ -10,17 +10,17 @@ import os
 from PyQt5.QtWidgets import QLineEdit
 
 
-def load_path(path, file):
+def load_path(path, _file):
     """
     Find a file in a given path string
     reference: https://github.com/yahoo/TensorFlowOnSpark/blob/e2f5cc45f95812d163e75b6ddb9c4661261d3bb0/tensorflowonspark/util.py#L57
 
     :param path: base path of the file
-    :param file: the name of the file, include postfix
+    :param _file: the name of the file, include postfix
     :return: if exist file, return the path of the file, otherwise, return False
     """
     for p in path.split(os.pathsep):
-        candidate = os.path.join(p, file)
+        candidate = os.path.join(p, _file)
     if os.path.exists(candidate) and os.path.isfile(candidate):
         return candidate
     return False

a971065401b50fd33f66b234daeba42a295692ce
loganballard <33067676+loganballard@users.noreply.github.com>
2019-08-21 23:17:10 -0700

    restructuing again

--- src/integrationtests/python/steps/step_login.py
+++ src/integrationtests/python/steps/step_login.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import Qt
 
 from behave import use_step_matcher, when, then
 
-from src.integration_tests.steps.step_helpers import assert_equal, assert_is_not
+from src.integrationtests.python.steps.step_helpers import assert_equal, assert_is_not
 
 use_step_matcher("re")
 

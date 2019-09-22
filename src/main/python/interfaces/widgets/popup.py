5bfaba03bd97e6e9011123390898ea690f6d405c
Fenting Lin <fen@pdx.edu>
2019-09-21 17:23:21 -0700

    reverse the code

--- src/main/python/interfaces/widgets/popup.py
+++ src/main/python/interfaces/widgets/popup.py
@@ -104,3 +104,117 @@ class Question(BaseDialog):
         
         self.reject()
         self.close()
+
+
+class Notification(BaseDialog):
+
+    def __init__(self, cxt:ApplicationContext, *args, **kwargs):
+        super(Notification, self).__init__(cxt, name="notification")
+
+        self.clean = None
+        self.done = None
+        self.window = None
+
+        self.setFixedSize(724, 510)
+        self._init_ui()
+
+    def _init_ui(self):
+        section_layout = VerticalLayout(self, t_m=5, b_m=5, l_m=5, r_m=5, space=5)
+
+        # title frame
+        title_frame = Frame(self, name="title_frame")
+        title_layout = HorizontalLayout(title_frame, space=1)
+        section_layout.addWidget(title_frame)
+
+        title = Label(
+            title_frame, text="Notification", align=Qt.AlignVCenter
+        )
+        title_layout.addWidget(title)
+
+        spacer = HorizontalSpacer()
+        title_layout.addItem(spacer)
+
+        self.clean = Button(title_frame, text="CLEAN", cursor=True)
+        title_layout.addWidget(self.clean)
+
+        self.done = Button(title_frame, text="DONE", cursor=True)
+        title_layout.addWidget(self.done)
+
+        # window frame
+
+        self.window = Frame(self)
+        section_layout.addWidget(self.window)
+
+        self.done.clicked.connect(self.on_done_clicked)
+
+    def on_done_clicked(self):
+        self.accept()
+        self.close()
+
+
+class CreditHistory(BaseDialog):
+
+    title_section: Frame = None
+    window_section: Frame = None
+
+    def __init__(self, cxt:ApplicationContext, *args, **kwargs):
+        super(CreditHistory, self).__init__(cxt, name="credit")
+
+        self.done = None
+        self.cost = None
+        self.profit = None
+
+        self.setFixedSize(724, 510)
+        self._init_ui()
+
+    def on_done_clicked(self):
+        self.accept()
+        self.close()
+
+    def _init_ui(self):
+        section_layout = VerticalLayout(self, t_m=5, b_m=5, l_m=5, r_m=5, space=5)
+
+        # title
+        self.title_section = Frame(self, name="title_frame")
+        section_layout.addWidget(self.title_section)
+        self._init_title_section()
+
+        # window
+        self.window_section = Frame(self)
+        section_layout.addWidget(self.window_section)
+        self._init_window_section()
+
+        self.done.clicked.connect(self.on_done_clicked)
+
+    def _init_window_section(self):
+
+        window_layout = VerticalLayout(self.window_section)
+
+        button_frame = Frame(self.window_section, name="button_frame")
+        button_layout = HorizontalLayout(button_frame, space=20)
+        window_layout.addWidget(button_frame)
+
+        self.cost = Button(button_frame, text="COST", name="button_active", cursor=True)
+        button_layout.addWidget(self.cost)
+
+        self.profit = Button(button_frame, text="PROFIT", name="button", cursor=True)
+        button_layout.addWidget(self.profit)
+
+        spacer = HorizontalSpacer()
+        button_layout.addItem(spacer)
+
+        self.window = Frame(self.window_section, name="window")
+        window_layout.addWidget(self.window)
+
+
+    def _init_title_section(self):
+        title_layout = HorizontalLayout(self.title_section)
+
+        title = Label(self.title_section, text="Recent 30 Days Transit Record", align=Qt.AlignVCenter)
+        title_layout.addWidget(title)
+
+        spacer = HorizontalSpacer()
+        title_layout.addItem(spacer)
+
+        self.done = Button(self.title_section, text="DONE", cursor=True)
+        title_layout.addWidget(self.done)

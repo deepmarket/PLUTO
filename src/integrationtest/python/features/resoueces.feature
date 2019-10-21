b39afdfacf4ebaff05045b6da2b61ca9936c3467
Fenting Lin <fen@pdx.edu>
2019-10-21 15:12:33 -0700

    finish add resource button test

--- src/integrationtest/python/features/resoueces.feature
+++ src/integrationtest/python/features/resoueces.feature
@@ -3,16 +3,20 @@
 Feature: # Verify functionality on resources tab
    # This inlcueds everything from clicking on the resources tab
 
-  Scenario: Click on Resources tab
-    When I click on the Resources tab
-    Then the window will switch to resources page
+  Scenario Outline: Verify the application stands up
+    When I spin up the application
+      And I open the login window
+      And I login in to the application
+      And I open the application window
+      And I click on the resources tab
+    Then the current tab should be the resources tab
 
   Scenario: Click on Add Button
-    When I click on the Resources tab
+    When I spin up the application
+      And I open the login window
+      And I login in to the application
+      And I open the application window
+      And I click on the resources tab
       And I click on the Add button
     Then the window will switch to add resource page
-
-  Scenario: Verify no input hint
-    When I click on the Resources tab
-      And I click on the Add button
-      And I click on the Submit button
+    
--- src/integrationtest/python/steps/step_resources.py
+++ src/integrationtest/python/steps/step_resources.py
@@ -2,7 +2,7 @@
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 from dashboard import Dashboard
-from resources import Resources
+from resources import Resources, ResourcesAddView
 from jobs import Jobs
 from settings import Settings
 from behave import use_step_matcher, when, then
@@ -10,24 +10,31 @@ from step_helpers import assert_equal, assert_is_not, assert_is_true, assert_is_
 use_step_matcher("re")
 
 
-@when(r'I open the resources window')
-def open_application(context):
-    context.resources_window = context.__app.resources
-    assert_is_not(context.app, None)
+@then(r'I open the resources window')
+def open_resources(context):
+    context.app = context.__app.app
 
+    check_type = lambda type_: assert_equal(type(context.app.main_window.stack.currentWidget()), type_)
+    assert_is_not(check_type(Resources), True)
+    
 
 @when(r'I click on the Add button')
-def add_resources(context, button):
-    assert_is_not(context.login_window, None)
+def add_resources(context):
+    context.resources_window = context.app.main_window.stack.currentWidget()
 
-    QTest.mouseClick(context.app.resources.add, Qt.LeftButton)
+    QTest.mouseClick(context.resources_window.controller.add, Qt.LeftButton)
 
 @then(r'the window will switch to add resource page')
 def verify_add_button(context):
-    assert_equal(context.resources_window.add_view.result(), QDialog.Accepted)
+    check_type = lambda type_: assert_equal(type(context.resources_window._stack.currentWidget()), type_)
 
+    assert_is_not(check_type(ResourcesAddView), True)
+    
+    
+"""
 @when(r'I click on the Submit button')
 def submit_resource(context, button):
     assert_is_not(context.login_window.add_view, None)
 
     QTest.mouseClick(context.app.resources.add_view.submit, Qt.LeftButton)
+"""
